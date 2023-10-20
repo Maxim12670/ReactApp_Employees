27ac862b37b25d0833d9184765dae1cbde5776e8
Максим <akaikinmax@mail.ru>
2023-10-20 15:37:31 +0700

    feature: добавлен функционал показа сотрудников

--- employees/src/components/app/app.js
+++ employees/src/components/app/app.js
@@ -9,64 +9,96 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [
-        { name: "Акайкин Максим", salary: 500, increase: false, id: 1 },
-        { name: "Желнов Никита", salary: 600, increase: false, id: 2 },
-        { name: "Лунев Лев", salary: 900, increase: true, id: 3 }
-      ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: "Акайкин Максим", salary: 500, increase: false, like: true, id: 1 },
+                { name: "Желнов Никита", salary: 600, increase: false, like: false, id: 2 },
+                { name: "Лунев Лев", salary: 900, increase: true, like: false, id: 3 }
+            ]
+        }
+        this.maxIndex = 4;
     }
-    this.maxIndex = 4;
-  }
 
-  deleteItem = (id) => {
-    this.setState(({ data }) => {
-      const index = data.findIndex(elem => elem.id === id);
-      const before = data.slice(0, index);
-      const after = data.slice(index + 1);
-      const newArray = [...before, ...after];
+    deleteItem = (id) => {
+        this.setState(({ data }) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+            const newArray = [...before, ...after];
 
-      return {
-        data: newArray
-      }
-    })
-  }
-
-  addItem = (name, salary) => {
-    const newItem = {
-      name, salary,
-      increase: false,
-      id: this.maxIndex++
+            return {
+                data: newArray
+            }
+        })
     }
-    this.setState(({data}) => {
-      const newArray = [...data, newItem];
-      return {
-        data: newArray
-      }
-    });
-  }
 
-  render() {
-    return (
-      <div className="app">
-        <AppInfo />
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            like: false,
+            id: this.maxIndex++
+        }
+        this.setState(({ data }) => {
+            const newArray = [...data, newItem];
+            return {
+                data: newArray
+            }
+        })
+    }
 
-        <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+    onToggleIncrease = (id) => {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const oldItem = data[index];
+            const newItem = {...oldItem, increase: !oldItem.increase};
+            const newArray = [...data.slice(0, index), newItem, ...data.slice(index+1)];
 
-        </div>
+            return {
+                data: newArray
+            }
+        })
+    }
 
-        <EmployeesList data={this.state.data}
-          onDelete={this.deleteItem} />
-        <EmployeesAddForm onAdd={this.addItem}/>
+    onToggleLike = (id) => {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const oldItem = data[index];
+            const newItem = {...oldItem, like: !oldItem.like};
+            const newArray = [...data.slice(0, index), newItem, ...data.slice(index+1)];
+            
+            return {
+                data: newArray
+            }
+        })
+    }
 
-      </div>
-    )
-  }
+    render() {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+
+        return (
+            <div className="app">
+                <AppInfo employees={employees} increased={increased}/>
+
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+
+                </div>
+
+                <EmployeesList data={this.state.data}
+                            onDelete={this.deleteItem} 
+                            onToggleIncrease={this.onToggleIncrease}
+                            onToggleLike={this.onToggleLike}/>
+                <EmployeesAddForm onAdd={this.addItem} />
+
+            </div>
+            )
+        }
 }
 
 export default App;
-
